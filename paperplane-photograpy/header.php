1324658098aaad81a25ca17507de82f002f47286
paperplanefactory <giovanni@paperplanefactory.com>
2020-01-09 17:33:01 +0100

    Set path to stylesheet directory as variable
    Set path to stylesheet directory as variable for favicons to saver resources.

--- paperplane-photograpy/header.php
+++ paperplane-photograpy/header.php
@@ -27,23 +27,24 @@
 // stabilisco il device
 $module_count = 0;
 global $module_count;
+$favicons_folder = get_stylesheet_directory_uri().'/images/favicons/';
 ?>
-<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/favicon-16x16.png">
-<link rel="manifest" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/manifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $favicons_folder; ?>apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $favicons_folder; ?>apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $favicons_folder; ?>apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $favicons_folder; ?>apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $favicons_folder; ?>apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $favicons_folder; ?>apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $favicons_folder; ?>apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $favicons_folder; ?>apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $favicons_folder; ?>apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $favicons_folder; ?>android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $favicons_folder; ?>favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo $favicons_folder; ?>favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $favicons_folder; ?>favicon-16x16.png">
+<link rel="manifest" href="<?php echo $favicons_folder; ?>manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?php bloginfo('stylesheet_directory'); ?>/images/favicons/ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="<?php echo $favicons_folder; ?>ms-icon-144x144.png">
 <style>
 body {
   font-family: <?php the_field( 'selettore_font', 'option' ); ?>;
