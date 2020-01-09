7071c3e209cfe6085d2480f6d5e7b0bfe14e30a3
paperplanefactory <giovanni@paperplanefactory.com>
2020-01-09 16:36:06 +0100

    Avoid activation errors
    Prevent theme activation if ACF PRO is not active.

--- paperplane-photograpy/functions.php
+++ paperplane-photograpy/functions.php
@@ -1,4 +1,41 @@
 <?php
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+add_action( 'after_switch_theme', 'check_theme_dependencies', 10, 2 );
+function check_theme_dependencies( $oldtheme_name, $oldtheme ) {
+  if ( !is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+
+    // Update default admin notice: Theme not activated.
+    add_filter( 'gettext', 'update_activation_admin_notice', 10, 3 );
+
+    // Custom styling for default admin notice.
+    add_action( 'admin_head', 'error_activation_admin_notice' );
+
+    // Switch back to previous theme.
+    switch_theme( $oldtheme->stylesheet );
+      return false;
+}
+}
+
+function update_activation_admin_notice( $translated, $original, $domain ) {
+    // Strings to translate.
+    $strings = array(
+        'New theme activated.' => 'Theme not activated. Before activating this theme please install and activate Advanced Custom Fields PRO.'
+    );
+
+    if ( isset( $strings[$original] ) ) {
+        // Translate but without running all the filters again.
+        $translations = get_translations_for_domain( $domain );
+        $translated = $translations->translate( $strings[$original] );
+    }
+
+    return $translated;
+}
+
+function error_activation_admin_notice() {
+  echo '<style>#message2{border-left-color:#dc3232;}</style>';
+}
+
+
 // imposto la versione del tema
 global $theme_version;
 if( class_exists('acf') ) {
