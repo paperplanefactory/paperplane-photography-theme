54274f8105c4bbfbe9e57571b91dbfb9b70cc625
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-01-11 11:05:47 +0100

    indentation

--- functions.php
+++ functions.php
@@ -1,34 +1,29 @@
 <?php
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 add_action( 'after_switch_theme', 'check_theme_dependencies', 10, 2 );
 function check_theme_dependencies( $oldtheme_name, $oldtheme ) {
+  include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
   if ( !is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-
     // Update default admin notice: Theme not activated.
     add_filter( 'gettext', 'update_activation_admin_notice', 10, 3 );
-
     // Custom styling for default admin notice.
     add_action( 'admin_head', 'error_activation_admin_notice' );
-
     // Switch back to previous theme.
     switch_theme( $oldtheme->stylesheet );
-      return false;
-}
+    return false;
+  }
 }
 
 function update_activation_admin_notice( $translated, $original, $domain ) {
-    // Strings to translate.
-    $strings = array(
-        'New theme activated.' => 'Theme not activated. Before activating this theme please install and activate Advanced Custom Fields PRO.'
-    );
-
-    if ( isset( $strings[$original] ) ) {
-        // Translate but without running all the filters again.
-        $translations = get_translations_for_domain( $domain );
-        $translated = $translations->translate( $strings[$original] );
-    }
-
-    return $translated;
+  // Strings to translate.
+  $strings = array(
+    'New theme activated.' => 'Theme not activated. Before activating this theme please install and activate Advanced Custom Fields PRO.'
+  );
+  if ( isset( $strings[$original] ) ) {
+    // Translate but without running all the filters again.
+    $translations = get_translations_for_domain( $domain );
+    $translated = $translations->translate( $strings[$original] );
+  }
+  return $translated;
 }
 
 function error_activation_admin_notice() {
@@ -36,22 +31,22 @@ function error_activation_admin_notice() {
 }
 
 
-// imposto la versione del tema
+// set theme version
 global $theme_version;
 if( class_exists('acf') ) {
   $theme_version = get_field( 'theme_version', 'option' );
 }
-// gestione caricamento css
+// CSS loading management
 include_once "functions/theme-stylesloader.php";
-// gestione caricamento script
+// scrips loading management
 include_once "functions/theme-scriptsloader.php";
-// gestione immagini
+// images loading
 include_once "functions/theme-images.php";
 // lazy load
 include_once "functions/theme-lazyload.php";
 // gestione trim testi
 include_once "functions/theme-txts.php";
-// gestione core WordPress
+// WordPress admin stuff
 include_once "functions/theme-messages.php";
 // custom menus
 include_once "functions/theme-menus.php";
--- functions/theme-txts.php
+++ functions/theme-txts.php
@@ -1,27 +1,4 @@
 <?php
-// page title generator
-function twentytwelve_wp_title( $title, $sep ) {
-	global $paged, $page;
-
-	if ( is_feed() )
-		return $title;
-
-	// Add the site name.
-	$title .= get_bloginfo( 'name' );
-
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title = "$title $sep $site_description";
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentytwelve' ), max( $paged, $page ) );
-
-	return $title;
-}
-add_filter( 'wp_title', 'twentytwelve_wp_title', 10, 2 );
-
 /**
  * Replaces the login header logo URL
  *
