56ae0beb46c1de94222ea24afa5216e5153e2049
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-02-06 00:27:20 +0100

    Damn me!
    Scripts path set with get_stylesheet_directory_uri instead of get_template_directory_uri

--- includes/theme-scriptsloader.php
+++ includes/theme-scriptsloader.php
@@ -27,32 +27,32 @@ function all_scripts(){
   // smart jquery inclusion
   if (!is_admin()) {
   	wp_deregister_script('jquery');
-  	wp_register_script('jquery', get_stylesheet_directory_uri() . '/assets/js/libs/jquery-3.4.1.min.js', '', '3.4.1', false);
+  	wp_register_script('jquery', get_template_directory_uri() . '/assets/js/libs/jquery-3.4.1.min.js', '', '3.4.1', false);
   	wp_enqueue_script('jquery');
   }
   // Swup
   // documentazione: https://github.com/swup/swup
-  wp_register_script( 'js-swup', get_stylesheet_directory_uri() . '/assets/js/libs/swup.min.js#deferload', '', $theme_version, false);
+  wp_register_script( 'js-swup', get_template_directory_uri() . '/assets/js/libs/swup.min.js#deferload', '', $theme_version, false);
   wp_enqueue_script( 'js-swup' );
-  wp_register_script( 'js-swup-head', get_stylesheet_directory_uri() . '/assets/js/libs/SwupHeadPlugin.min.js#deferload', '', $theme_version, true);;
+  wp_register_script( 'js-swup-head', get_template_directory_uri() . '/assets/js/libs/SwupHeadPlugin.min.js#deferload', '', $theme_version, true);;
   wp_enqueue_script( 'js-swup-head' );
-  //wp_register_script( 'js-swup-ga', get_stylesheet_directory_uri() . '/assets/js/libs/SwupGaPlugin.min.js#deferload', '', $theme_version, true);;
+  //wp_register_script( 'js-swup-ga', get_template_directory_uri() . '/assets/js/libs/SwupGaPlugin.min.js#deferload', '', $theme_version, true);;
   //wp_enqueue_script( 'js-swup-ga' );
-  wp_register_script( 'js-swup-preload', get_stylesheet_directory_uri() . '/assets/js/libs/SwupPreloadPlugin.min.js#deferload', '', $theme_version, true);;
+  wp_register_script( 'js-swup-preload', get_template_directory_uri() . '/assets/js/libs/SwupPreloadPlugin.min.js#deferload', '', $theme_version, true);;
   wp_enqueue_script( 'js-swup-preload' );
   // Infinite Scroll
   // documentazione: https://infinite-scroll.com/
-  wp_register_script( 'custom-infinitescroll', get_stylesheet_directory_uri() . '/assets/js/libs/infinite-scroll.pkgd.min.js#deferload', '', '3.0.6', false);
+  wp_register_script( 'custom-infinitescroll', get_template_directory_uri() . '/assets/js/libs/infinite-scroll.pkgd.min.js#deferload', '', '3.0.6', false);
   wp_enqueue_script( 'custom-infinitescroll' );
   // Lazy load
   // documentazione: http://www.andreaverlicchi.eu/lazyload/
-  wp_register_script( 'vanilla-lazyload', get_stylesheet_directory_uri() . '/assets/js/libs/lazyload.min.js#deferload', '', '12.3.0', false);
+  wp_register_script( 'vanilla-lazyload', get_template_directory_uri() . '/assets/js/libs/lazyload.min.js#deferload', '', '12.3.0', false);
   wp_enqueue_script( 'vanilla-lazyload' );
 	// Comportamenti ricorrenti
-	wp_register_script( 'theme-general', get_stylesheet_directory_uri() . '/assets/js/theme-general.min.js#deferload', '', $theme_version, true);
+	wp_register_script( 'theme-general', get_template_directory_uri() . '/assets/js/theme-general.min.js#deferload', '', $theme_version, true);
 	wp_enqueue_script( 'theme-general' );
   // tocca
-  wp_register_script( 'theme-tocca', get_stylesheet_directory_uri() . '/assets/js/libs/tocca.min.js#asyncload', '', $theme_version, true);
+  wp_register_script( 'theme-tocca', get_template_directory_uri() . '/assets/js/libs/tocca.min.js#asyncload', '', $theme_version, true);
 	wp_enqueue_script( 'theme-tocca' );
   // FontAwesome
   // documentazione: https://fontawesome.com/
