49cb0e492d024a33bada8b92413cea8849a6e659
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-04 18:49:51 +0200

    Fix image sizes

--- template-parts/grid/post.php
+++ template-parts/grid/post.php
@@ -57,7 +57,7 @@
        $image_data = array(
            'image_type' => 'post_thumbnail', // options: post_thumbnail, acf_field, acf_sub_field
            'image_value' => '', // se utilizzi un custom field indica qui il nome del campo
-           'size_fallback' => 'grid_image_crop_2cols'
+           'size_fallback' => 'grid_image_mobile_crop'
        );
        $image_sizes = array( // qui sono definiti i ritagli o dimensioni. Devono corrispondere per numero a quanto dedinfito nella funzione nei parametri data-srcset o srcset
            'desktop_retina' => 'grid_image_retina_crop_2cols',
