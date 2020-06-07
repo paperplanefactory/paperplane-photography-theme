717c134058c77a8e577c226bce1031a57b257304
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-07 23:55:22 +0200

    Improved service worker

--- assets/pwa/sw.js
+++ assets/pwa/sw.js
@@ -1,80 +1,141 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+/* global self, caches, fetch, URL, Response */
+'use strict';
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+var config = {
+  version: 'achilles',
+  staticCacheItems: [
+    'wp-content/themes/paperplane-photography-theme/style.min.css',
+    '/',
+    '?utm_source=pwa-homescreen',
+    'wp-content/themes/paperplane-photography-theme/assets/js/theme-general.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/infinite-scroll.pkgd.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/masonry.pkgd.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/swup.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/SwupHeadPlugin.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/SwupPreloadPlugin.min.js',
+    'wp-content/themes/paperplane-photography-theme/assets/js/libs/tocca.min.js'
+  ],
+  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  offlineImage: '<svg role="img" aria-labelledby="offline-title"' +
+    ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">' +
+    '<title id="offline-title">Offline</title>' +
+    '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>' +
+    '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">' +
+    '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlinePage: '/offline/'
+};
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'wp-content/themes/paperplane-photography-theme/style.min.css',
-  './',
-  '?utm_source=pwa-homescreen',
-  'wp-content/themes/paperplane-photography-theme/assets/js/theme-general.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/infinite-scroll.pkgd.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/masonry.pkgd.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/swup.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/SwupHeadPlugin.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/SwupPreloadPlugin.min.js',
-  'wp-content/themes/paperplane-photography-theme/assets/js/libs/tocca.min.js'
-];
+function cacheName(key, opts) {
+  return `${opts.version}-${key}`;
+}
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-    .then(cache => cache.addAll(PRECACHE_URLS))
-    .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+function addToCache(cacheKey, request, response) {
+  if (response.ok) {
+    var copy = response.clone();
+    caches.open(cacheKey).then(cache => {
+      cache.put(request, copy);
+    });
   }
+  return response;
+}
+
+function fetchFromCache(event) {
+  return caches.match(event.request).then(response => {
+    if (!response) {
+      throw Error(`${event.request.url} not found in cache`);
+    }
+    return response;
+  });
+}
+
+function offlineResponse(resourceType, opts) {
+  if (resourceType === 'image') {
+    return new Response(opts.offlineImage, {
+      headers: {
+        'Content-Type': 'image/svg+xml'
+      }
+    });
+  } else if (resourceType === 'content') {
+    return caches.match(opts.offlinePage);
+  }
+  return undefined;
+}
+
+self.addEventListener('install', event => {
+  function onInstall(event, opts) {
+    var cacheKey = cacheName('static', opts);
+    return caches.open(cacheKey)
+      .then(cache => cache.addAll(opts.staticCacheItems));
+  }
+
+  event.waitUntil(
+    onInstall(event, config).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  function onActivate(event, opts) {
+    return caches.keys()
+      .then(cacheKeys => {
+        var oldCacheKeys = cacheKeys.filter(key => key.indexOf(opts.version) !== 0);
+        var deletePromises = oldCacheKeys.map(oldKey => caches.delete(oldKey));
+        return Promise.all(deletePromises);
+      });
+  }
+
+  event.waitUntil(
+    onActivate(event, config)
+    .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+
+  function shouldHandleFetch(event, opts) {
+    var request = event.request;
+    var url = new URL(request.url);
+    var criteria = {
+      matchesPathPattern: opts.cachePathPattern.test(url.pathname),
+      isGETRequest: request.method === 'GET',
+      isFromMyOrigin: url.origin === self.location.origin
+    };
+    var failingCriteria = Object.keys(criteria)
+      .filter(criteriaKey => !criteria[criteriaKey]);
+    return !failingCriteria.length;
+  }
+
+  function onFetch(event, opts) {
+    var request = event.request;
+    var acceptHeader = request.headers.get('Accept');
+    var resourceType = 'static';
+    var cacheKey;
+
+    if (acceptHeader.indexOf('text/html') !== -1) {
+      resourceType = 'content';
+    } else if (acceptHeader.indexOf('image') !== -1) {
+      resourceType = 'image';
+    }
+
+    cacheKey = cacheName(resourceType, opts);
+
+    if (resourceType === 'content') {
+      event.respondWith(
+        fetch(request)
+        .then(response => addToCache(cacheKey, request, response))
+        .catch(() => fetchFromCache(event))
+        .catch(() => offlineResponse(resourceType, opts))
+      );
+    } else {
+      event.respondWith(
+        fetchFromCache(event)
+        .catch(() => fetch(request))
+        .then(response => addToCache(cacheKey, request, response))
+        .catch(() => offlineResponse(resourceType, opts))
+      );
+    }
+  }
+  if (shouldHandleFetch(event, config)) {
+    onFetch(event, config);
+  }
+
 });
