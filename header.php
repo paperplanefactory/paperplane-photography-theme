5278fb8253b93aa35b32c8ec7a3f1a0ccce59a59
paperplanefactory <giovanni@paperplanefactory.com>
2021-05-08 11:55:50 +0200

    Added options to set custom colors scheme

--- header.php
+++ header.php
@@ -55,6 +55,12 @@ $custom_logo_dark = get_field( 'custom_logo_dark', 'options' );
 $start_color_scheme = get_field( 'start_color_scheme', 'options' );
 $show_abstract = get_field( 'show_abstract', 'options' );
 $show_cats = get_field( 'show_cats', 'options' );
+if ( get_field( 'custom_colors', 'options' ) ) {
+  $custom_colors = get_field( 'custom_colors', 'options' );
+}
+else {
+  $custom_colors = 0;
+}
 ?>
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="<?php echo $favicons_folder; ?>ms-icon-144x144.png">
@@ -77,7 +83,7 @@ $show_cats = get_field( 'show_cats', 'options' );
   // If service workers are supported, and one isn't already registered
   if ('serviceWorker' in navigator && !navigator.serviceWorker.controller) {
     navigator.serviceWorker.register('/sw.js');
-    console.log("sw.js loaded...");
+    //console.log("sw.js loaded...");
   }
   </script>
 <?php endif; ?>
@@ -135,8 +141,445 @@ input[type=submit],
 a:link, a:visited, a:hover, a:active {
   color: #191919;
 }
+<?php if ( $custom_colors == 1 ) : ?>
+body.clear-theme {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  background-color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
 
+body.clear-theme .eye i {
+  background-color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
 
+.clear-theme div,
+.clear-theme footer,
+.clear-theme header,
+.clear-theme li,
+.clear-theme nav,
+.clear-theme ul {
+  border-color: <?php the_field( 'custom_color_2', 'options' ); ?> !important;
+}
+
+.clear-theme .logo-dark {
+  display: none;
+}
+
+.clear-theme input[type],
+.clear-theme textarea {
+  border-color: <?php the_field( 'custom_color_3', 'options' ); ?> !important;
+}
+
+.clear-theme ::-moz-selection {
+  background: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme ::selection {
+  background: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme ::-moz-selection {
+  background: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme ::-webkit-selection {
+  background: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme ::-webkit-input-placeholder {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme :-moz-placeholder {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme ::-moz-placeholder {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme #paperplane-cookie-notice {
+  background-color: <?php the_field( 'custom_color_1', 'options' ); ?> !important;
+  color: <?php the_field( 'custom_color_4', 'options' ); ?> !important;
+}
+
+.clear-theme #paperplane-cookie-notice a {
+  background-color: <?php the_field( 'custom_color_4', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?> !important;
+}
+
+.clear-theme .paperplane-gdpr-content-message {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme .open_head .hambuger-element span {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?> !important;
+}
+
+.clear-theme .hambuger-element span {
+  background: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .hambuger-element:hover span {
+  background: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .txt-1 {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .txt-2 {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .txt-3 {
+  color: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .txt-4 {
+  color: <?php the_field( 'custom_color_2', 'options' ); ?>;
+}
+
+.clear-theme .txt-5 {
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme .category-list:before {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .content-styled ul li:before {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme ol > li:before {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .bg-1 {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .bg-2 {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .bg-3 {
+  background-color: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .bg-4 {
+  background-color: <?php the_field( 'custom_color_2', 'options' ); ?>;
+}
+
+.clear-theme .bg-5 {
+  background-color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+}
+
+.clear-theme a:link,
+.clear-theme a:visited {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme a:hover {
+  color: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .current-menu-item a,
+.clear-theme .current-page-ancestor a {
+  color: <?php the_field( 'custom_color_3', 'options' ); ?> !important;
+}
+
+.clear-theme .navi-info {
+  color: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .navi-info a:link,
+.clear-theme .navi-info a:visited {
+  color: <?php the_field( 'custom_color_3', 'options' ); ?>;
+}
+
+.clear-theme .navi-info a:hover {
+  color: <?php the_field( 'custom_color_4', 'options' ); ?>;
+}
+
+.clear-theme .highlight:hover,
+.clear-theme .list:hover {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .txt-4:link, .clear-theme .txt-4:visited {
+  color: <?php the_field( 'custom_color_4', 'options' ); ?>;
+}
+
+.clear-theme .txt-4:hover {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .pay-picture button {
+  color: #64D31C;
+}
+
+.clear-theme .flex-hold-title {
+  background-color: rgba(255, 255, 255, 0.85);
+}
+
+.clear-theme .sk-folding-cube .sk-cube:before {
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .form-hold label {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .form-hold input[type=email],
+.clear-theme .form-hold input[type=number],
+.clear-theme .form-hold input[type=tel],
+.clear-theme .form-hold input[type=text] {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .form-hold textarea {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .form-hold select {
+  color: <?php the_field( 'custom_color_2', 'options' ); ?>;
+}
+
+.clear-theme .form-hold input[type=submit] {
+  color: <?php the_field( 'custom_color_1', 'options' ); ?>;
+  background-color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+}
+
+.clear-theme .form-hold input[type=submit]:hover {
+  color: <?php the_field( 'custom_color_5', 'options' ); ?>;
+  background-color: <?php the_field( 'custom_color_2', 'options' ); ?>;
+}
+<?php else : ?>
+body.clear-theme {
+  color: #303030;
+  background-color: #FFFFFF;
+}
+
+body.clear-theme .eye i {
+  background-color: #FFFFFF;
+}
+
+.clear-theme div,
+.clear-theme footer,
+.clear-theme header,
+.clear-theme li,
+.clear-theme nav,
+.clear-theme ul {
+  border-color: #F9F9F9 !important;
+}
+
+.clear-theme .logo-dark {
+  display: none;
+}
+
+.clear-theme input[type],
+.clear-theme textarea {
+  border-color: #AFAFAF !important;
+}
+
+.clear-theme ::-moz-selection {
+  background: #303030;
+  color: #FFFFFF;
+}
+
+.clear-theme ::selection {
+  background: #303030;
+  color: #FFFFFF;
+}
+
+.clear-theme ::-moz-selection {
+  background: #303030;
+  color: #FFFFFF;
+}
+
+.clear-theme ::-webkit-selection {
+  background: #303030;
+  color: #FFFFFF;
+}
+
+.clear-theme ::-webkit-input-placeholder {
+  color: #303030;
+}
+
+.clear-theme :-moz-placeholder {
+  color: #303030;
+}
+
+.clear-theme ::-moz-placeholder {
+  color: #303030;
+}
+
+.clear-theme #paperplane-cookie-notice {
+  background-color: #FFFFFF !important;
+  color: #4D4D4D !important;
+}
+
+.clear-theme #paperplane-cookie-notice a {
+  background-color: #4D4D4D;
+  color: #FFFFFF !important;
+}
+
+.clear-theme .paperplane-gdpr-content-message {
+  background-color: #303030;
+  color: #FFFFFF;
+}
+
+.clear-theme .open_head .hambuger-element span {
+  background-color: #303030 !important;
+}
+
+.clear-theme .hambuger-element span {
+  background: #303030;
+}
+
+.clear-theme .hambuger-element:hover span {
+  background: #AFAFAF;
+}
+
+.clear-theme .txt-1 {
+  color: #303030;
+}
+
+.clear-theme .txt-2 {
+  color: #303030;
+}
+
+.clear-theme .txt-3 {
+  color: #AFAFAF;
+}
+
+.clear-theme .txt-4 {
+  color: #F9F9F9;
+}
+
+.clear-theme .txt-5 {
+  color: #FFFFFF;
+}
+
+.clear-theme .category-list:before {
+  background-color: #303030;
+}
+
+.clear-theme .content-styled ul li:before {
+  color: #303030;
+}
+
+.clear-theme ol > li:before {
+  color: #303030;
+}
+
+.clear-theme .bg-1 {
+  background-color: #303030;
+}
+
+.clear-theme .bg-2 {
+  background-color: #303030;
+}
+
+.clear-theme .bg-3 {
+  background-color: #AFAFAF;
+}
+
+.clear-theme .bg-4 {
+  background-color: #F9F9F9;
+}
+
+.clear-theme .bg-5 {
+  background-color: #FFFFFF;
+}
+
+.clear-theme a:link,
+.clear-theme a:visited {
+  color: #303030;
+}
+
+.clear-theme a:hover {
+  color: #AFAFAF;
+}
+
+.clear-theme .current-menu-item a,
+.clear-theme .current-page-ancestor a {
+  color: #AFAFAF !important;
+}
+
+.clear-theme .navi-info {
+  color: #AFAFAF;
+}
+
+.clear-theme .navi-info a:link,
+.clear-theme .navi-info a:visited {
+  color: #AFAFAF;
+}
+
+.clear-theme .navi-info a:hover {
+  color: #4D4D4D;
+}
+
+.clear-theme .highlight:hover,
+.clear-theme .list:hover {
+  color: #303030;
+}
+
+.clear-theme .txt-4:link, .clear-theme .txt-4:visited {
+  color: #4D4D4D;
+}
+
+.clear-theme .txt-4:hover {
+  color: #303030;
+}
+
+.clear-theme .pay-picture button {
+  color: #64D31C;
+}
+
+.clear-theme .flex-hold-title {
+  background-color: rgba(255, 255, 255, 0.85);
+}
+
+.clear-theme .sk-folding-cube .sk-cube:before {
+  background-color: #303030;
+}
+
+.clear-theme .form-hold label {
+  color: #303030;
+}
+
+.clear-theme .form-hold input[type=email],
+.clear-theme .form-hold input[type=number],
+.clear-theme .form-hold input[type=tel],
+.clear-theme .form-hold input[type=text] {
+  color: #303030;
+}
+
+.clear-theme .form-hold textarea {
+  color: #303030;
+}
+
+.clear-theme .form-hold select {
+  color: #F9F9F9;
+}
+
+.clear-theme .form-hold input[type=submit] {
+  color: #FFFFFF;
+  background-color: #303030;
+}
+
+.clear-theme .form-hold input[type=submit]:hover {
+  color: #303030;
+  background-color: #F9F9F9;
+}
+<?php endif; ?>
 </style>
 <script type="text/javascript">
 var show_play_pause_button_localstorage = "<?php echo $show_play_pause_button_localstorage ?>";
@@ -279,7 +722,6 @@ var start_color_scheme = "<?php echo $start_color_scheme ?>";
     </div>
   </div>
 </div>
-
 <div class="swupped">
   <div class="mouse-trap">
     <div class="body-shaper">
